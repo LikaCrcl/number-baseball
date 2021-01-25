6b3fc7b7c83d56c9fab7af0ebaa8e6d444e8b418
hyeonmo_ <ahgusdl288@gmail.com>
2021-01-25 21:09:32 +0900

    feat(add-scanner) : 스캐너 클래스 추가

---
+++ src/NumberBaseball.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class NumberBaseball {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String number = scanner.next();
+
+
+    }
+
+}
